fc13df1e521a28ceafca169242fb0f9cc029eb0e
HeyAlomgir <alomgirhosssain71@gmail.com>
2026-04-17 09:52:31 +0600

    filtaring added

--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -1,24 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { TimelineFried } from '../../context/TimelineFreind';
 import { LuPhoneCall } from 'react-icons/lu';
 import { FaRegCommentDots } from 'react-icons/fa';
 import { FcNoVideo } from 'react-icons/fc';
 
 const Timeline = () => {
-       const {audio}=useContext(TimelineFried);
-       const  {text}=useContext(TimelineFried);
-       const   {vedeo}=useContext(TimelineFried);
+
+       const {audio,text,vedeo}=useContext(TimelineFried);
+    
+
+       const [selected,setSelcted]=useState("all");
 
 
 
     return (
-        <div className='container mx-auto'>
-            <div>
+      
+        <div className='container mx-auto '>
+
+          <div className='py-4'>
                 <h1 className='text-black text-3xl font-bold'>TimeLine</h1>
             </div>
 
+
+
+      {/* filter Dropdown */}
+            <select onChange={(e)=>setSelcted(e.target.value)} className='p-2 border rounded'>
+              <option value="all">Filter TimeLine</option>
+              <option value="call">Call</option>
+              <option value="text">Text</option>
+              <option value="video">Videos</option>
+            </select>
+
+          {(audio.length===0 && text.length===0 && vedeo.length === 0 )?
+           <div className='text-center py-45 space-y-5'>
+            <h2 className="text-4xl font-bold text-gray-800">Your Timeline is Empty</h2>
+             <p className="text-gray-500 mt-2 px-4">
+            No audio calls, text messages, or videos have been added to your timeline yet.
+        </p>
+
+           </div>:
+           ""}
+
+            
+            
+
             {/* Audio */}
-              {
+
+            
+            {(selected === "call" || selected === "all" ) && (
+              
+              
                 audio.map((call,ind)=>{
                     return (
                         <div key={ind} className='card bg-base-100 shadow rounded-2xl w-full py-5 my-5 '>
@@ -34,11 +65,16 @@ const Timeline = () => {
           </div>
                     )
                 })
-              }
+              
+            )}
+
+
 
                 {/* Text */}
-              <div>
-              {
+              
+              {(selected === "text" || selected==="all") && (
+            
+              
                 text.map((call,ind)=>{
                     return (
                         <div key={ind} className='card bg-base-100 shadow rounded-2xl w-full py-5 my-5 '>
@@ -54,14 +90,15 @@ const Timeline = () => {
           </div>
                     )
                 })
-              }
-              </div>
+              
+              
+              )}
+
 
               {/* Vedeo */}
 
-             <div>
-                 {
-                vedeo.map((call,ind)=>{
+            {(selected==="video" || selected ==="all") && (
+                   vedeo.map((call,ind)=>{
                     return (
                         <div key={ind} className='card bg-base-100 shadow rounded-2xl w-full py-5 my-5 '>
 
@@ -76,8 +113,11 @@ const Timeline = () => {
           </div>
                     )
                 })
-              }
-             </div>
+            )}
+                 
+           
+              
+             
 
 
 
